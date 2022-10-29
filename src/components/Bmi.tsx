5cd58c0062a7a048a0df3cc51018428e8b1f4259
Camilo Suarez <camiloandressuarez1@gmail.com>
2022-10-29 18:36:36 -0500

    create BMI calculator  --- practice/ ts-lab

--- src/components/Bmi.tsx
+++ src/components/Bmi.tsx
@@ -1,9 +1,5 @@
 import { ChangeEvent, FormEvent, useState } from "react";
 
-const initialState = {
-  weigth: '',
-  height: ''
-}
 
 export const Bmi = () => {
 
@@ -17,12 +13,13 @@ export const Bmi = () => {
 
    */
 
-  const[values, setValues]= useState(initialState)
+  const[values, setValues]= useState({
+    weight: 0,
+    height: 0
+  })
   const[total, setTotal] = useState('')
-  const [value1, setValue1] = useState<number>(0)
-  const [value2, setValue2] = useState<number>(0)
-
-
+  const [value1, setValue1] = useState(0)
+  const [value2, setValue2] = useState(0)
 
 /*   const bmi = (weight: number, height: number)=> {
     console.log(weight / height);
@@ -30,45 +27,55 @@ export const Bmi = () => {
 
   bmi(22,33) */
 
-
-  const calculateBmi = (e: FormEvent<HTMLFormElement>) =>{
-    e.preventDefault()
-    setTotal(bmi(value1, value2))
-  }
+ 
   
-  const bmi = (value1: number, value2:number)=> {
-    return (value1 / ((value2 * value2)/ 10000)).toFixed(2)
-  }
+  const bmi = (value1 / ((value2 * value2)/ 10000)).toFixed(2)
+
+ /*  const handleChange = (evt:ChangeEvent<HTMLInputElement>)=> {
+    setValues({
+      ...values, [evt.target.name]: evt.target.value
+      
+    })
+  } */
+
+  const handleWeight = (e:ChangeEvent<HTMLInputElement>)=>{
+    setValue1(parseInt(e.target.value))
+  } 
+
 
   const handleHeight = (e:ChangeEvent<HTMLInputElement>)=>{
     setValue2(parseFloat(e.target.value))
   }
-  
-  const handleWeight = (e:ChangeEvent<HTMLInputElement>)=>{
-    setValue1(parseFloat(e.target.value))
+
+  const calculateBmi = (e:FormEvent<HTMLFormElement>) =>{
+    e.preventDefault()
+    setTotal(bmi)
   }
 
 
   return (
     <main className="border-2 border-slate-600 p-4 border-solid max-w-md m-auto">
       <form onSubmit={calculateBmi}  className="flex flex-col items-center py-2 text-slate-500" >
-      <h1 className=" text-3xl text-center text-slate-400 font-bold pb-4">BMC Calculator</h1>
+        <h1 className=" text-3xl text-center text-slate-400 font-bold pb-4">BMC Calculator</h1>
+        <input
+        className="my-2 p-2 w-full bg-slate-700 border-2  border-slate-500 "
+        type="number"
+        name="weight"
+        value={value1}
+        placeholder="Your Weight in Kg"
+        onChange={handleWeight}
+        />
+
       <input
-      className="my-2 p-2 w-full bg-slate-700 border-2  border-slate-500 "
-      type="number"
-      name="weigth"
-      value={value1}
-      placeholder="Your Weight in Kg"
-      onChange={handleWeight}/>
-      
-      <input
-      className="my-2 p-2 w-full bg-slate-700 border-2  border-slate-500 "
-      type="number"
-      name="height"
-      value={value2}
-      placeholder="Your Height in cm"
-      onChange={handleHeight}/>
-      <button type="button" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 mt-4">Calculte</button>
+        className="my-2 p-2 w-full bg-slate-700 border-2  border-slate-500 "
+        type="number"
+        name="height"
+        value={value2}
+        placeholder="Your Height in cm"
+        onChange={handleHeight}
+        />
+
+        <button type="submit" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 mt-4">Calculte</button>
       </form>
       <h1>TOTAL IS: {total}</h1>
     </main>
